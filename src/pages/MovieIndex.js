7c1bb0531350d8189c5d2797044367863d377fae
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-09-13 18:02:32 +0800

    update movie lists component

--- src/pages/MovieIndex.js
+++ src/pages/MovieIndex.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import MovieLists from '../pages/Movies/MovieLists.js'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import MovieCard from './Movies/component/MovieCard'
+import '../styles/css/component/MovieLists.css'
 
 const MovieIndex = () => {
+	const moviesData1 = [{
+		title: 'James Bond',
+		image_url: 'https://www.reportdoor.com/wp-content/uploads/2020/06/No-Time-to-Die-Daniel-Craigs-James-Bond-Movie-Gets-990x550.jpg',
+		description: 'Adipisicing illo neque aperiam ullam ut. Expedita impedit reprehenderit magnam rem exercitationem! Sit ratione obcaecati consequatur ea dolorem. Quidem laudantium quasi ipsa omnis architecto. Ea quasi cumque est veniam soluta',
+		rating: 9,
+		duration: 120,
+		genre: 'action'
+	}]
+	const [moviesData, setMoviesData] = useState([])
+
+	useEffect( () => {
+		if(moviesData.length === 0){
+			axios.get(`http://backendexample.sanbercloud.com/api/movies`)
+			.then(res=>{
+				setMoviesData(res.data)
+			})
+		}
+	})
+
 	return(
 		<>
 			<h1>Daftar Film Terbaik</h1>
-			<MovieLists/>
+			<div className="movies-container">
+				{moviesData.map((data, index) => {
+					return(
+						<MovieCard key={index} data={data}/>
+					)
+				})}
+			</div>
 		</>
 	)
 }
